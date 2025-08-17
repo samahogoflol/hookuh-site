1f459dbca1b19c23705abe4717987ef5b676c9b2
Denys <denis.shmatkov95@gmail.com>
2025-08-17 14:01:29 +0300

    bag fixed

---
+++ src/data/coalContent.ts
@@ -0,0 +1,79 @@
+// import coalImg from "../imgs/coals/brands/ugol-logo.webp";
+// import khramaCocoLoco72_1kg_without_wrapper from "../imgs/coals/coals/ugol_khmara_coco_loco_1kg_bez_yp.webp";
+// import mindAirGap72_1kg from "../imgs/coals/coals/mind_air_gap_1kg_korobka_jpg.webp";
+// import mindAirGap72_1kg_without_wrapper from "../imgs/coals/coals/ugol_khmara_coco_loco_1kg_bez_yp.webp";
+
+// export const coalTypeOfProductContent: Product[] = [
+//   {
+//     id: "0",
+//     name: "Вугілля Горіхове",
+//     imgUrl: coalImg,
+//   },
+//   {
+//     id: "1",
+//     name: "Вугілля Кокосове",
+//     imgUrl: coalImg,
+//   },
+// ];
+
+// export const coalProductContent: CoalProduct[] = [
+//   {
+//     id: "0",
+//     brand: " Coco Loco",
+//     name: " Вугілля Кокосове (72 кубики) (Без Упаковки) 1кг",
+//     description: "Якщо у вас залишилися питання, ви завжди можете їх задати нам за номером телефону +38(050)844-95-00.",
+//     imgUrl: khramaCocoLoco72_1kg_without_wrapper,
+//     price: 270,
+//     having: true,
+//     about: {
+//       type: "Кокосовий",
+//       weight: "1",
+//       sizeOfcube: "25х25х25",
+//       producer: "Khmara",
+//       formOfCube: "Кубик",
+//       quantity: 72,
+//       timeToCorruption: "від 60 до 80",
+//       package: "Целофан",
+//     },
+//   },
+//   {
+//     id: "1",
+//     brand: " Mind Air Gap",
+//     name: " Вугілля Горіхове В Упаковці (72 Кубика) 1 кг",
+//     description: "Якщо у вас залишилися питання, ви завжди можете їх задати нам за номером телефону +38(050)844-95-00.",
+//     imgUrl: mindAirGap72_1kg,
+//     price: 190,
+//     having: true,
+//     about: {
+//       type: "Горіховий",
+//       weight: "1",
+//       sizeOfcube: "25х25х25",
+//       producer: "Mind",
+//       country: "Україна",
+//       formOfCube: "Кубик",
+//       quantity: 72,
+//       timeToCorruption: "від 50 до 70",
+//       package: "Фірмова Коробка",
+//     },
+//   },
+//   {
+//     id: "2",
+//     brand: " Mind Air Gap",
+//     name: " Вугілля Горіхове (72 Кубика) 1 кг",
+//     description: "Якщо у вас залишилися питання, ви завжди можете їх задати нам за номером телефону +38(050)844-95-00.",
+//     imgUrl: mindAirGap72_1kg_without_wrapper,
+//     price: 170,
+//     having: true,
+//     about: {
+//       type: "Горіховий",
+//       weight: "1",
+//       sizeOfcube: "25х25х25",
+//       producer: "Mind",
+//       country: "Україна",
+//       formOfCube: "Кубик",
+//       quantity: 72,
+//       timeToCorruption: "від 50 до 70",
+//       package: "Целофан",
+//     },
+//   },
+// ];
