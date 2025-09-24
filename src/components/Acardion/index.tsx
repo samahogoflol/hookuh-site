4d7b41b651eaee958dbd5d9ce8b92b4f4e7ad826
denys <denis.shmatkov95@gmail.com>
2025-09-24 11:38:49 +0300

    test commit

--- src/components/Acardion/index.tsx
+++ src/components/Acardion/index.tsx
@@ -6,6 +6,7 @@ interface AcardionProps {
 const Acardion: React.FC<AcardionProps> = ({ title, description }) => {
   return (
     <>
+    <h2>test changes</h2>
       <section className="shadow-lg p-6 mt-10">
         <h2 className="text-2xl font-medium  mb-7">
           <span className="opacity-30 font-bold">+</span> {title}
